947e53ae9bc134c101a2b057ad3848940ef47c5c
t.abe@aegis.dip.jp <t.abe.aegis@gmail.com>
2019-07-26 11:02:22 +0900

    サンプル追加

---
+++ Jenkinsfile
@@ -0,0 +1,19 @@
+pipeline {
+	agent any
+
+	stages {
+		stage('job1'){
+			echo "job1"
+		}
+
+		stage('job2'){
+			echo "job2"
+		}
+
+		stage('job3'){
+			echo "job3"
+		}
+	}
+}
+
+
---
+++ libcitool.groovy
@@ -0,0 +1,43 @@
+
+
+// パラメータに沿ってstageを実行する
+def __exec_stage(def stage_param)
+{
+    // ディレクトリをクリアし、ファイルをマスターから転送する。
+    deleteDir()
+    if (stage_param.unstash != null) {
+        unstash 'initialize'
+    } else {
+        unstash stage_param.unstash.name
+    }
+
+    script.each { __line ->
+        if (__line.sh != null) {
+            sh __line.sh
+        }
+    }
+
+    // 結果の保存指示があった場合は、stashを行う。
+    if (stage_param.stash != null) {
+        stash stage_param.stash
+    }
+    
+}
+
+def __mk_parallel(def parallel_list, def stage_list)
+{
+    def parallel = [:]
+    parallel_list.each { oneline ->
+        parallel[oneline] = {
+            node (stage_list[oneline].node){
+                __exec_stage(stage_list[oneline])
+          }
+        }
+    }
+    return parallel
+}
+
+stage_list = [job1:[node:"master", sh:"make", unstash:"projects"], job2:[node:"master", sh:"make"]]
+parallel_list = ["job1", "job2"]
+
+__mk_parallel(parallel_list, stage_list)
